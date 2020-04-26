9e19f88b8375cfa589a49bb86e57661e62932038
Anton <64253585+Tonny161@users.noreply.github.com>
2020-04-26 20:47:27 +0300

    Третий комит

--- use.js
+++ use.js
@@ -24,7 +24,33 @@ for (let i = 0; i < 2; i++) {
     }
 }
 
-appData.moneyPerDay = appData.budget / 30;
+appData.moneyPerDay = appData.budget / 30; 
 
 alert('Ежедневный бюджет: ' + appData.moneyPerDay);
 
+if(appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Произошла ошибка");
+}  
+
+
+/*switch (appData.moneyPerDay) {
+    case appData.moneyPerDay < 100:
+        console.log("Минимальный уровень достатка");
+        break;
+    case appData.moneyPerDay > 100 && appData.moneyPerDay < 2000:  
+        console.log("Средний уровень достатка");
+        break
+    case appData.moneyPerDay > 2000:
+        console.log("Высокий уровень достатка");
+        break
+    default:
+        console.log("Произошла ошибка");
+        break
+}    
+*/
